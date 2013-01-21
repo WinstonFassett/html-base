3552a9680457e1a09f70d997c0bc8eb71ccfe299
Winston Fassett <winston.fassett@gmail.com>
2013-01-21 17:26:16 -0600

    removed collapse button, changed title

--- app/samples/bootstrap/shell.js
+++ app/samples/bootstrap/shell.js
@@ -1,6 +1,6 @@
 define(['require','knockout', 'spa/router', 'ux/showPane', 'crossroads'], function(require, ko, router, showPane, crossroads){
 	
-    var title_ = ko.observable('You can change this');
+    var title_ = ko.observable('Boostrap App!');
     
     require(['css!ux/panes.css']);    
     require(['css!lib/bootstrap/css/bootstrap.css']);
